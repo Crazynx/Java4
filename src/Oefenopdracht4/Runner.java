5ea46b11f178ac4bfd8f623eec5db025664a8fe2
Crazynx <123321@student.drenthecollege.nl>
2019-05-22 14:06:43 +0200

    added comments to runner

--- src/Oefenopdracht4/Runner.java
+++ src/Oefenopdracht4/Runner.java
@@ -4,12 +4,23 @@ import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+    /*
+     * Runner
+     * The runner is responsible for running the program. It also sets the title, scene and other attributes.
+     */
+
 public class Runner extends Application {
 
+    /*
+     * Fallback method
+     */
     public static void main (String[] args) {
         launch(args);
     }
 
+    /*
+     * This method starts the application.
+     */
     @Override
     public void start(Stage primaryStage) throws Exception {
         Scene scene = new Scene(new Controller(), 500, 200);
