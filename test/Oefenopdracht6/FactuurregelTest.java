64deb42681eb426e068f41c7949b5ca8bd732cd5
Crazynx <123321@student.drenthecollege.nl>
2019-05-23 19:35:46 +0200

    started with factuurregel test case

--- test/Oefenopdracht6/FactuurregelTest.java
+++ test/Oefenopdracht6/FactuurregelTest.java
@@ -1,5 +1,35 @@
+package Oefenopdracht6;
+
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class FactuurregelTest {
+
+    @Test
+    void setFactuurregel() {
+        // Setup
+        Factuurregel factuurregel = new Factuurregel();
+        Product product = new Product();
+        product.setPrijs(10);
+        product.setOmschrijving("houten hamer");
+
+        // Kijk of er in ieder geval iets is veranderd, de regel mag niet leeg zijn
+        assertNotEquals("", product.getFactuurregel);
+    }
+
+    @Test
+    void getFactuurregel() {
+        Factuurregel factuurregel = new Factuurregel();
+        Product product = new Product();
+
+        product.setPrijs(10.00);
+        product.setOmschrijving("houten hamer");
+
+        factuurregel.setFactuurregel(2, product);
+
+        String verwachteOutput = "2 | houten hamer | Prijs: €10.00 excl. BTW";
+        assertEquals(verwachteOutput, factuurregel.getFactuurregel);
+    }
 
 }
--- test/Oefenopdracht6/ProductTest.java
+++ test/Oefenopdracht6/ProductTest.java
@@ -1,5 +1,12 @@
+package Oefenopdracht6;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class ProductTest {
+
+
 
 }
