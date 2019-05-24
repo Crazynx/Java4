cdd105db97c0bbd54ea6465447684d99553c0670
Lars van Vliet <123321@student.drenthecollege.nl>
2019-05-24 22:06:47 +0200

    changed factuurregel according to definition and test cases

--- src/Oefenopdracht6/Factuurregel.java
+++ src/Oefenopdracht6/Factuurregel.java
@@ -2,4 +2,16 @@ package Oefenopdracht6;
 
 public class Factuurregel {
 
+    String factuurregel;
+
+    public void setFactuurregel(int aantal, Product product) {
+        factuurregel = aantal + " | " + product.getOmschrijving() +
+                "\nPer stuk: €" + product.getPrijs() + " Excl. BTW (" + product.getBelasting() + ")" +
+                "\nTotaal: €" + product.getPrijs() * aantal * (product.getBelasting()/100 + 1) +
+                " Incl. BTW (" + product.getBelasting() + ")";
+    }
+
+    public String getFactuurregel() {
+        return factuurregel;
+    }
 }
