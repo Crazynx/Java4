f837ba850577b2bdd4308eedb78ec91e111ee164
Crazynx <123321@student.drenthecollege.nl>
2019-05-28 15:21:03 +0200

    calculator that can add, subtract, divide and multiply. Also retrieves answer

--- src/Eindopdracht/Calculator.java
+++ src/Eindopdracht/Calculator.java
@@ -1,4 +1,27 @@
 package Eindopdracht;
 
 public class Calculator {
+
+    private double answer;
+
+    public void add(double first, double second) {
+        answer = first + second;
+    }
+
+    public void subtract(double first, double second) {
+        answer = first + second;
+    }
+
+    public void multiply(double first, double second) {
+        answer = first * second;
+    }
+
+    public void divide(double first, double second) {
+        answer = first / second;
+    }
+
+    public double getAnswer() {
+        previousAnswer = answer;
+        return answer;
+    }
 }
