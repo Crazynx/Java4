bb3460b4e583ddc2e1e9bd0857a7e09f2bf1fd52
Crazynx <123321@student.drenthecollege.nl>
2019-05-28 17:59:21 +0200

    test for setNumber method

--- test/Eindopdracht/ModelTest.java
+++ test/Eindopdracht/ModelTest.java
@@ -1,5 +1,36 @@
+package Eindopdracht;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class ModelTest {
 
+    Model model;
+
+    @BeforeEach
+    void setup() {
+        model = new Model();
+    }
+
+    @Test
+    void setNumber() {
+        model.setNumber(1);
+        model.setNumber(2);
+        model.setSeparator();
+        model.setNumber(3);
+        model.setNumber(5);
+        assertEquals(12.35, model.getFirstNumber());
+
+    }
+
+    @Test
+    void setSeparator() {
+    }
+
+    @Test
+    void setOperator() {
+    }
 }
