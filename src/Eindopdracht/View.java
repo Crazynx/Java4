2b2b2059bf0e22c28af7ced637d6284a820497b9
Crazynx <123321@student.drenthecollege.nl>
2019-05-28 17:57:08 +0200

    added output screen

--- src/Eindopdracht/View.java
+++ src/Eindopdracht/View.java
@@ -1,4 +1,37 @@
 package Eindopdracht;
 
-public class View {
+import javafx.geometry.Pos;
+import javafx.scene.control.Label;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.TextAlignment;
+
+public class View extends HBox {
+
+    private Label resultLabel;
+
+    public View() {
+        initResultLabel();
+    }
+
+    private void initResultLabel() {
+        resultLabel = new Label();
+        resultLabel.setText("");
+        resultLabel.setAlignment(Pos.CENTER);
+        resultLabel.setPrefWidth(300);
+        resultLabel.setPrefHeight(75);
+        resultLabel.setFont(Font.font ("Consolas", FontWeight.BOLD, 30));
+        resultLabel.setStyle("-fx-background-color:#e0e0e0");
+        this.setAlignment(Pos.CENTER);
+        this.getChildren().add(resultLabel);
+    }
+
+    public void setText(String text) {
+        resultLabel.setText(text);
+    }
+
 }
