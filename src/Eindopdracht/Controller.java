35629f67925f817eb357494fba55423f3deda3b2
Crazynx <123321@student.drenthecollege.nl>
2019-05-29 14:31:50 +0200

    removed switchcase and replaced it with a single line that does the same

--- src/Eindopdracht/Controller.java
+++ src/Eindopdracht/Controller.java
@@ -32,30 +32,14 @@ public class Controller extends VBox implements PropertyChangeListener {
 
     @Override
     public void propertyChange(PropertyChangeEvent evt) {
-        switch (evt.getPropertyName()) {
-            case "answer":
-                view.setText(evt.getNewValue().toString());
-                break;
-
-            case "operator":
-                view.setText(evt.getNewValue().toString());
-                break;
-
-            case "reset":
-                view.setText(evt.getNewValue().toString());
-                break;
-        }
+            view.setText(evt.getNewValue().toString());
     }
 
-    private void initRows() {
-        firstRow = new HBox();
-        secondRow = new HBox();
-        thirdRow = new HBox();
-        fourthRow = new HBox();
-        this.getChildren().add(firstRow);
-        this.getChildren().add(secondRow);
-        this.getChildren().add(thirdRow);
-        this.getChildren().add(fourthRow);
+    private void initRows() { // create and add new rows to controller
+        this.getChildren().add(firstRow = new HBox());
+        this.getChildren().add(secondRow = new HBox());
+        this.getChildren().add(thirdRow = new HBox());
+        this.getChildren().add(fourthRow = new HBox());
     }
 
     private void initNumberButtons() {
@@ -65,8 +49,9 @@ public class Controller extends VBox implements PropertyChangeListener {
         for (int x = 0; x < 4; x++) {
             for (int y = 0; y < 3; y++) {
                 if (index < 10) {
-                    Button button = new Button();
-                    button.setText(numbers[index]);
+                    Button button = new Button(numbers[index]);
+                    button.setPrefWidth(75);
+                    button.setPrefHeight(75);
                     int finalIndex = index;
                     button.setOnAction(event ->
                             model.setNumber(Integer.parseInt(numbers[finalIndex]))
@@ -82,8 +67,9 @@ public class Controller extends VBox implements PropertyChangeListener {
         char[] operators = {'/', 'x', '-', '+'};
         HBox[] rows = {firstRow, secondRow, thirdRow, fourthRow};
         for (int x = 0; x < 4; x++) {
-            Button button = new Button();
-            button.setText(String.valueOf(operators[x]));
+            Button button = new Button(String.valueOf(operators[x]));
+            button.setPrefWidth(75);
+            button.setPrefHeight(75);
             int finalX = x;
             button.setOnAction(event ->
                 model.setOperator(operators[finalX])
@@ -96,16 +82,18 @@ public class Controller extends VBox implements PropertyChangeListener {
     private void initOtherButtons() {
         char[] others = {'.', '='};
         // '.' button
-        Button button = new Button();
-        button.setText(String.valueOf(others[0]));
+        Button button = new Button(String.valueOf(others[0]));
+        button.setPrefWidth(75);
+        button.setPrefHeight(75);
         button.setOnAction(event -> {
             model.setSeparator();
         });
         fourthRow.getChildren().add(button);
 
         // '=' button
-        button = new Button();
-        button.setText(String.valueOf(others[1]));
+        button = new Button(String.valueOf(others[1]));
+        button.setPrefWidth(75);
+        button.setPrefHeight(75);
         button.setOnAction(event -> {
             model.setAnswer();
         });
